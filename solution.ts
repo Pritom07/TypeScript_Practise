c456e3fe0648497f158ae67415609c28f3e57bf5
Pritom07 <pritommohajon4545@gmail.com>
2025-11-19 00:24:56 +0600

    Problem 3 done

--- solution.ts
+++ solution.ts
@@ -10,3 +10,17 @@ const getLength = (value: paramType2): number | undefined => {
   if (typeof value === "string") return value.length;
   if (Array.isArray(value)) return value.length;
 };
+
+class Person {
+  public name: string;
+  public age: number;
+  public constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  public getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+const person1 = new Person("John Doe", 30);
+const person2 = new Person("Alice", 25);
