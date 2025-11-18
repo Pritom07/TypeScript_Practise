33488a5712a5b069608731658925f406c084d4a6
Pritom07 <pritommohajon4545@gmail.com>
2025-11-19 00:10:53 +0600

    Problem 2 done

--- solution.ts
+++ solution.ts
@@ -1,6 +1,12 @@
-type paramType = number | string | boolean;
-const formatValue = (value: paramType): paramType => {
+type paramType1 = number | string | boolean;
+const formatValue = (value: paramType1): paramType1 => {
   if (typeof value === "string") return value.toUpperCase();
   else if (typeof value === "number") return value * 10;
   else return !value;
+};
+
+type paramType2 = string | Array<number> | Array<string>;
+const getLength = (value: paramType2): number | undefined => {
+  if (typeof value === "string") return value.length;
+  if (Array.isArray(value)) return value.length;
 };
